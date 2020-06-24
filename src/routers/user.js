525b0628141c026259ee4cc6e7288c6b635b39bc
ImmArkorful <e.arkorful@gmail.com>
2020-06-24 23:17:28 +0000

    test

--- src/routers/user.js
+++ src/routers/user.js
@@ -13,8 +13,8 @@ router.post('/users', async (req,res) => {
 
     try{
         await user.save();
+        sendWelcomeEmail(user.email, user.name)
         const token = await user.generateAuthToken();
-        // sendWelcomeEmail(user.email, user.name)
         res.status(201).send({user , token});
     } catch(e){
         res.status(400).send(e);
@@ -59,21 +59,7 @@ router.get('/users/me', auth, async (req,res) => {
     res.send(req.user);
 })
 
-/* router.get('/users/:id', async (req,res) => {
-    const _id = req.params.id;
 
-    try{
-        const user = await User.findById(_id);
-
-    if(!user){
-        return res.status(404).send()
-    }
-
-    res.status(200).send(user);
-    } catch (e) {
-        res.status(500).send(e);
-    } 
-}) */
 
 router.patch('/users/me', auth, async (req,res) => {
     const updates = Object.keys(req.body)
@@ -86,11 +72,6 @@ router.patch('/users/me', auth, async (req,res) => {
     try{
         updates.forEach((update) => req.user[update] = req.body[update]);
         await req.user.save();
-
-        // const user = await User.findByIdAndUpdate(req.params.id, req.body, { 
-        //     new: true, 
-        //     runValidators: true
-        // })
         res.status(200).send(req.user);
     }catch (e) {
         res.status(400).send(e);
@@ -99,11 +80,6 @@ router.patch('/users/me', auth, async (req,res) => {
 
 router.delete('/users/me', auth, async (req,res) => {
     try {
-        // const user = await User.findByIdAndDelete(req.user._id)
-
-        // if(!user){
-        //     return res.status(404).send();
-        // }
         sendDeleteEmail(req.user.email, req.user.name);
         await req.user.remove()
         res.send(req.user);
@@ -113,7 +89,6 @@ router.delete('/users/me', auth, async (req,res) => {
 })
 
 const upload = multer({
-    // dest: 'avatars',
     limits: {
         fileSize: 1000000
     },
