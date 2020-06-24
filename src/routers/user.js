3ee36ece1c92b78ff1a15ffa6f1bd7567f3aa540
ImmArkorful <e.arkorful@gmail.com>
2020-06-24 21:56:08 +0000

    test

--- src/routers/user.js
+++ src/routers/user.js
@@ -14,7 +14,7 @@ router.post('/users', async (req,res) => {
     try{
         await user.save();
         const token = await user.generateAuthToken();
-        sendWelcomeEmail(user.email, user.name)
+        // sendWelcomeEmail(user.email, user.name)
         res.status(201).send({user , token});
     } catch(e){
         res.status(400).send(e);
